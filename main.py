b09c3d11a157e836983ab2847fa22cdff88f8aff
EvanSnyder2023 <816547@mystma.org>
2021-02-03 16:39:52 +0000

    reversal try 1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def reversal(w):
+
+    reverse = w[len (w) - 1]
+    for i in range(2, len(w)+ 1):
+        reverse = reverse + w[len (w) - i]
+
+    return reverse
+
+print(reversal("Chicken"))
